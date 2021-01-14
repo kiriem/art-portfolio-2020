d4337647b602bb060c32e703e80b5bed0ace5bf9
宮島衣瑛 <kiriem@KYRIEs-Mac-mini.local>
2021-01-14 18:43:02 +0900

    First commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  
+    <link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-main-blue">
+        <a class="navbar-brand" href="#">KYRIE's Portfolio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+            
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="jumbotron text-center">
+        <h1 class="display-5">美術教育事例研究 Portfolio</h1>
+        <p class="lead">学習院大学大学院人文科学研究科教育学専攻 美術教育事例研究2020 最終成果物</p>
+        <hr class="my-4">
+        <p>教育学専攻M1 宮島 衣瑛 @kiriem_</p>
+    </div>
+
+    <div class="container-fluid">
+    
+        <div class="row bg-main-white p-4">
+        
+            <div class="col-md-8 offset-md-2">
+            
+                <h1 class="main-title text-center mt-4">コンセプト</h1>
+                <p class="text my-5">前回このポートフォリオを作ったのは、学部2年生のときだった。当時のポートフォリオを見ると、「テクノロジーを使ったあたらしい表現」をテーマにコンピュータを使って作成している。今回、このポートフォリオを超えるためにはどのような
+                工夫をしたらよいだろうかと考えていた。1つは、この3年間のうちに上達したDTPスキルを駆使して、再度同じようにポートフォリオを作成することである。しかし、Covid-19下においてますます需要が高まっているインターネット上での表現に最適化された形で表現したほうが、時代性にあっているといえるだろう。<br>
+                私の研究テーマは、コンピュータを使った表現を子どもたちに体験してもらうことでもある。子どもたちは、紙やペンなどの固定化されたメディアだけでなく、コンピュータという強力な新しいメディアを通した表現活動をしてほしいと願っている。そんな私自身がまずは率先してWebポートフォリオという形を取ってみたい。</p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</body>
+</html>
